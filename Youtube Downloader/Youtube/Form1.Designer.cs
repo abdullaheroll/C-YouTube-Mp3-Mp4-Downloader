ee8469e46b5e8548ab2b7807df2e34027829a84c
Abdullah <info@abdullaherol.com>
2021-11-07 18:41:41 +0300

    C# Youtube Mp3/Mp4 İndirici

---
+++ Youtube Downloader/Youtube/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Youtube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.baslik = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonMP4 = new System.Windows.Forms.RadioButton();
+            this.radioButtonMP3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CadetBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(78, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(391, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(78, 300);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(391, 37);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // baslik
+            // 
+            this.baslik.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.baslik.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baslik.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.baslik.Location = new System.Drawing.Point(38, 9);
+            this.baslik.Name = "baslik";
+            this.baslik.Size = new System.Drawing.Size(699, 63);
+            this.baslik.TabIndex = 5;
+            this.baslik.Text = "YOUTUBE MP3 - MP4 İNDİRİCİ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(4, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "URL:";
+            // 
+            // radioButtonMP4
+            // 
+            this.radioButtonMP4.AutoSize = true;
+            this.radioButtonMP4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButtonMP4.ForeColor = System.Drawing.Color.Coral;
+            this.radioButtonMP4.Location = new System.Drawing.Point(372, 151);
+            this.radioButtonMP4.Name = "radioButtonMP4";
+            this.radioButtonMP4.Size = new System.Drawing.Size(97, 36);
+            this.radioButtonMP4.TabIndex = 2;
+            this.radioButtonMP4.TabStop = true;
+            this.radioButtonMP4.Text = "MP4";
+            this.radioButtonMP4.UseVisualStyleBackColor = true;
+            this.radioButtonMP4.TextChanged += new System.EventHandler(this.radioButtonMP4_TextChanged);
+            // 
+            // radioButtonMP3
+            // 
+            this.radioButtonMP3.AutoSize = true;
+            this.radioButtonMP3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButtonMP3.ForeColor = System.Drawing.Color.Coral;
+            this.radioButtonMP3.Location = new System.Drawing.Point(78, 151);
+            this.radioButtonMP3.Name = "radioButtonMP3";
+            this.radioButtonMP3.Size = new System.Drawing.Size(97, 36);
+            this.radioButtonMP3.TabIndex = 1;
+            this.radioButtonMP3.TabStop = true;
+            this.radioButtonMP3.Text = "MP3";
+            this.radioButtonMP3.UseVisualStyleBackColor = true;
+            this.radioButtonMP3.TextChanged += new System.EventHandler(this.radioButtonMP3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(521, 349);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.baslik);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButtonMP4);
+            this.Controls.Add(this.radioButtonMP3);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "YouTube İndirici";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label baslik;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonMP4;
+        private System.Windows.Forms.RadioButton radioButtonMP3;
+    }
+}
+
---
+++ Youtube Downloader/Youtube/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MediaToolkit;
+using VideoLibrary;
+
+namespace Youtube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean format = true;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //YouTube MP3/MP4 İndirici
+            //github.com/abdullaheroll
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog() { Description = "Lütfen Klasör Seçiniz" })
+            {
+                if (dialog.ShowDialog()==DialogResult.OK)
+                {
+                    GetTitle();
+                    MessageBox.Show("İndirme İşlemi Başladı Lütfen Bekleyiniz...","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    var yt = YouTube.Default;
+                    var video = await yt.GetVideoAsync(textBox1.Text);
+                    File.WriteAllBytes(dialog.SelectedPath + @"\" + video.FullName, await video.GetBytesAsync());
+
+                    var girisdosyasi = new MediaToolkit.Model.MediaFile { Filename = dialog.SelectedPath + @"\" + video.FullName };
+                    var cikisdosyasi = new MediaToolkit.Model.MediaFile { Filename = $"{dialog.SelectedPath + @"\" + video.FullName }.mp3" };
+
+                    using (var enging = new Engine())
+                    {
+                        enging.GetMetadata(girisdosyasi);
+                        enging.Convert(girisdosyasi, cikisdosyasi);
+
+                    }
+                    if (format == true)
+                    {
+                        File.Delete(dialog.SelectedPath + @"\" + video.FullName);
+                    }
+                    if (format == false)
+                    {
+                        File.Delete($"{dialog.SelectedPath + @"\" + video.FullName}.mp3");
+                    }
+
+
+                    progressBar1.Value = 100;
+                    MessageBox.Show("İndirme İşlemi Tamamlandı <3", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Dosya Yolu Seçiniz...", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    var yt = YouTube.Default;
+                }
+            }
+        }
+
+
+
+
+        void GetTitle()
+        {
+            WebRequest istek = HttpWebRequest.Create(textBox1.Text);
+            WebResponse yanit = istek.GetResponse();
+            StreamReader sr = new StreamReader(yanit.GetResponseStream());
+            string gelen = sr.ReadToEnd();
+            int baslangic = gelen.IndexOf("<title>") + 7;
+            int bitis = gelen.Substring(baslangic).IndexOf("</title>");
+            string gelenbilgi = gelen.Substring(baslangic, bitis);
+            baslik.Text = (gelenbilgi);
+        }
+
+        private void radioButtonMP3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButtonMP4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButtonMP3_CheckedChanged(object sender, EventArgs e)
+        {
+            format = true;
+        }
+
+        private void radioButtonMP4_CheckedChanged(object sender, EventArgs e)
+        {
+            format = false;
+        }
+    }
+}
